e9d63b8595d54b2d60525f1f3a0a0348cf2ae896
Khorezmiy1991 <gm.khamza@gmail.com>
2021-05-09 16:13:39 +0500

    initial commit

---
+++ src/main/java/uz/pdp/Login.java
@@ -0,0 +1,28 @@
+package uz.pdp;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Login extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect("login.html");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        PrintWriter writer = resp.getWriter();
+        resp.setContentType("text/html");
+        writer.write("<h2>Successfully! " + username + "</h2>");
+        writer.write("<a href='/logout'>Logout</a>");
+
+    }
+}
